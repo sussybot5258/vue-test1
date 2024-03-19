d988acfbd350e68aa8bddbd55b62a6e90b7f7a69
sussybot5258 <jhx16901690@gmail.com>
2024-03-19 11:56:05 +0800

    add submodule type

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import type { UserConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx({})],
-  server: {
-    port: 8080,
-  },
-  resolve: {
-    alias: {
-      "@": Bun.fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig((env) => {
+  const config: UserConfig = {
+    plugins: [vue(), vueJsx({})],
+    server: {
+      port: 8080,
     },
-  },
-  build: {
-    assetsDir: "projects/test1/assets",
-  },
+    resolve: {
+      alias: {
+        "@": Bun.fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+  if (env.mode === "submodule") {
+    config.build = {
+      assetsDir: "projects/test1/assets",
+    };
+  }
+
+  return config;
 });
